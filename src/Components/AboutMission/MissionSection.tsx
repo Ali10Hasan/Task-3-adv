e7746bffa61379cfd8eb7f74532e02c385d43d43
Ali10Hasan <ah6306803@gmail.com>
2026-04-22 17:36:31 -0700

    Finish

--- src/Components/AboutMission/MissionSection.tsx
+++ src/Components/AboutMission/MissionSection.tsx
@@ -4,12 +4,12 @@ import MissionCard from "./MissionCard"
 const MissionCardInfo=[
   {
     title:"Mission",
-    icone:"/Task-2-adv/Mission1.png",
+    icone:"/Task-3-adv/Mission1.png",
     desc:"At Little Learners Academy, our mission is to inspire a passion for learning and empower young minds to become confident, compassionate, and creative individuals. We strive to create a safe and inclusive space where children thrive academically, socially, and emotionally, setting the stage for a successful educational journey."
   },
   {
     title:"Vision",
-    icone:"/Task-2-adv/Mission2.png",
+    icone:"/Task-3-adv/Mission2.png",
     desc:"Our vision is to be a beacon of educational excellence, where children are encouraged to explore, discover, and express their unique talents. We aim to foster a generation of lifelong learners equipped with critical thinking, empathy, and a deep appreciation for diversity."
   }
 ]
--- src/Components/AcademicsFeature/FeatureSection.tsx
+++ src/Components/AcademicsFeature/FeatureSection.tsx
@@ -10,32 +10,32 @@ export interface BenefitsCardType{
 }
 const FeatureCardInfo:Array<BenefitsCardType>=[
   {
-    icone:"/Task-2-adv/Feature1.png",
+    icone:"/Task-3-adv/Feature1.png",
     title:"Thematic Learning",
     description:"Our curriculum is centered around engaging themes that capture children's imaginations. Each theme integrates multiple subjects, making learning enjoyable and relevant."
   },
   {
-    icone:"/Task-2-adv/Feature2.png",
+    icone:"/Task-3-adv/Feature2.png",
     title:"STEAM Education",
     description:"We offer innovative STEAM (Science, Technology, Engineering, Arts, and Mathematics) programs that promote hands-on exploration, critical thinking, and problem-solving skills."
   },
   {
-    icone:"/Task-2-adv/Feature3.png",
+    icone:"/Task-3-adv/Feature3.png",
     title:"Language Immersion",
     description:"Through language immersion programs, children have the opportunity to learn a second language, enhancing cognitive development and global awareness  fostering ."
   },
   {
-    icone:"/Task-2-adv/Feature4.png",
+    icone:"/Task-3-adv/Feature4.png",
     title:"Art and Creativity",
     description:"Art is celebrated at Little Learners Academy. Our art-focused activities encourage self-expression, creativity, and the appreciation of various art forms"
   },
   {
-    icone:"/Task-2-adv/Feature5.png",
+    icone:"/Task-3-adv/Feature5.png",
     title:"Outdoor Education",
     description:"Our expansive outdoor learning spaces provide a stimulating environment for children to explore nature, fostering a connection with the environment."
   },
   {
-    icone:"/Task-2-adv/Feature6.png",
+    icone:"/Task-3-adv/Feature6.png",
     title:"Play-Based Learning",
     description:"Play is an integral part of early childhood education. Our play-based approach enhances social skills, emotional development, and imaginative thinking."
   },
